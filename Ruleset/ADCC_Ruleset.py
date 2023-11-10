4edf4d6258bd1dfab40dd2c700971dc79cbf6126
Zubair <zubair99ss@hotmail.com>
2023-11-10 15:57:28 +0800

    Create database queries for weight based on gender

--- Ruleset/ADCC_Ruleset.py
+++ Ruleset/ADCC_Ruleset.py
@@ -1,20 +1,61 @@
 """ This module provides Ruleset main module. """
 
+import sqlite3
+
 genders = ["male", "female"]
-weight = 0
 levels =["beginnner", "intermediate", "advanced", "professional"]
+weight = 0
+age = 0
+gender = ""
+level = ""
 
 class Ruleset:
-    def __init__(self, gender, weight, level):
-        self._competitor = _Competitor(gender, weight, level)
+    def __init__(self):
+        self._competitor = _Competitor(gender, weight, level, age)
 
-    
+    def display_rules(self):
+        #TODO: generate rules based on given data
+        age = _Competitor.get_age
+        gender = _Competitor.get_gender
+        weight = _Competitor.get_weight
+        level = _Competitor.get_level
+
+        competitor = _Competitor(gender, weight, age, level)
+
+    def determine_ruleset(competitor):
+            try:
+                conn = sqlite3.connect("C:\Users\zubai\OneDrive\Documents\Databases\ADCC_RULESET.db")
+                cursor = conn.cursor()
+            except(FileNotFoundError):
+                print("ERROR: Cannot connect to the database.")
+            
+            if (gender == "male"):
+                # Query the male weight categories
+                cursor.execute("""
+                        SELECT category
+                        FROM adults_male_weight_categories
+                        WHERE lower_limit <= ? AND (upper_limit IS NULL OR ? <= upper_limit)
+                    """, (weight, weight))
+            else:
+                # Query the female weight categories
+                cursor.execute("""
+                        SELECT category
+                        FROM adults_female_weight_categories
+                        WHERE lower_limit <= ? AND (upper_limit IS NULL OR ? <= upper_limit)
+                    """, (weight, weight))
+
+            if (competitor.age < 7):
+                print("Sorry you are unable to compete yet, come back when you are over 7 years old.")
+                exit()
+
+            
 
 class _Competitor:
-    def __init__(self, gender, weight, level):
+    def __init__(self, gender, weight, level, age):
         self._gender = gender
         self._weight = weight
         self._level = level
+        self.age = age
 
     def get_gender():
         while gender not in genders:
@@ -31,7 +72,7 @@ class _Competitor:
                 _Competitor.get_weight()
 
         except ValueError:
-            print("That is not a number. Please only enter positive numbers.")
+            print("ERROR: That is not a number. Please only enter positive numbers.")
         return weight
 
     def get_level():
@@ -40,5 +81,17 @@ class _Competitor:
                 level = input("Please enter your competitor level: ").lower()
                 if (level != "beginnner" or "intermediate" or "advanced" or "professional"):
                     print("Please only enter a valid competitor level ,this includes Beginner, Intermediate, Advanced and Professional.")
+            return level
         except (ValueError):
-            print("Please only enter a valid competitor level ,this includes Beginner, Intermediate, Advanced and Professional.")
+            print("ERROR: Please only enter a valid competitor level ,this includes Beginner, Intermediate, Advanced and Professional.")
+
+    def get_age():
+        try:
+            age = int(input("Please enter your age: "))
+            if (age >= 0):
+                print("Please only enter positive numbers.")
+                _Competitor.get_age()
+
+        except ValueError:
+            print("ERROR: That is not a valid input. Please only enter positive whole numbers.")
+        return age
--- Ruleset/cli.py
+++ Ruleset/cli.py
@@ -1,9 +1,23 @@
 """ This module provides the ruleset CLI."""
 
 try:
-    pass
+    import argparse
+    from . import __version__
+    from .ADCC_Ruleset import Ruleset
 except (ImportError):
     print("Failed to import necessary packages.")
 
 def main():
-    pass
+    ars = parse_cmd_line_arguments()
+    ruleset = Ruleset()
+
+def parse_cmd_line_arguments():
+    parser = argparse.ArgumentParser(
+        prog = "ruleset",
+        description= "ADCC Ruleset, generates appropriate rules that apply to the given competitor based on input.",
+        epilog= "Thanks for using the ADCC Ruleset Generator!"
+    )
+    parser.version = f"ADCC Ruleset Generator v{__version__}"
+    parser.add_argument("v", "--version", action="version")
+
+    return parser.parse_args()
