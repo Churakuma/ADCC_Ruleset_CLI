e05fadfa70d583cd78735cf9f49cf1b080137fcf
Zubair <zubair99ss@hotmail.com>
2023-11-10 14:25:45 +0800

    Add getter functions for basic competitor info

--- Ruleset/ADCC_Ruleset.py
+++ Ruleset/ADCC_Ruleset.py
@@ -8,6 +8,8 @@ class Ruleset:
     def __init__(self, gender, weight, level):
         self._competitor = _Competitor(gender, weight, level)
 
+    
+
 class _Competitor:
     def __init__(self, gender, weight, level):
         self._gender = gender
@@ -16,7 +18,27 @@ class _Competitor:
 
     def get_gender():
         while gender not in genders:
-            gender = input("Are you male or female?").lower()
+            gender = input("Please enter your gender: ").lower()
             if (gender != "male" or "female"):
                 print("Please answer as Male or Female.")
         return gender
+    
+    def get_weight():
+        try:
+            weight = float(input("Please enter your weight in Kilograms: "))
+            if (weight >= 0):
+                print("Please only enter positive numbers.")
+                _Competitor.get_weight()
+
+        except ValueError:
+            print("That is not a number. Please only enter positive numbers.")
+        return weight
+
+    def get_level():
+        try:
+            while level not in levels:
+                level = input("Please enter your competitor level: ").lower()
+                if (level != "beginnner" or "intermediate" or "advanced" or "professional"):
+                    print("Please only enter a valid competitor level ,this includes Beginner, Intermediate, Advanced and Professional.")
+        except (ValueError):
+            print("Please only enter a valid competitor level ,this includes Beginner, Intermediate, Advanced and Professional.")
--- Ruleset/cli.py
+++ Ruleset/cli.py
@@ -1,2 +1,9 @@
 """ This module provides the ruleset CLI."""
 
+try:
+    pass
+except (ImportError):
+    print("Failed to import necessary packages.")
+
+def main():
+    pass
